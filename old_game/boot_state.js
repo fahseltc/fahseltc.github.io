212a290ce3b5da01d233d803f72ff231b8a7ab4a
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-04-06 18:56:58 -0500

    replaced with better other site

--- old_game/boot_state.js
+++
@@ -1,9 +0,0 @@
-var boot_state = {
-    create: function() {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-        console.log('boot');
-        game.state.start('load');
-    }
-};
