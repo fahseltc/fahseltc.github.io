251b2b2470df5ff701b66fe4fcea07970e25384e
Charles Fahselt <digdugdiggy@gmail.com>
2025-03-12 10:35:59 -0600

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -5,24 +5,4 @@ module.exports =
 	function(eleventyConfig) {
 		eleventyConfig.addPassthroughCopy("css");
 		eleventyConfig.addPassthroughCopy("img");
-		// eleventyConfig.setBrowserSyncConfig({
-		// 	callbacks: {
-		// 		ready: function (err, bs) {
-		// 			bs.addMiddleware("*", (req, res) => {
-		// 				if (!fs.existsSync(NOT_FOUND_PATH)) {
-		// 					throw new Error(
-		// 						`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`
-		// 					);
-		// 				}
-
-		// 				const content_404 = fs.readFileSync(NOT_FOUND_PATH);
-		// 				// Add 404 http status code in request header.
-		// 				res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-		// 				// Provides the 404 content without redirect.
-		// 				res.write(content_404);
-		// 				res.end();
-		// 			});
-		// 		},
-		// 	},
-		// });
 	};
