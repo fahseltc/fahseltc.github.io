a133500ffafcc7f6c612172b45f7699867fe174f
fahseltc <fahseltc@users.noreply.github.com>
2025-03-21 03:30:01 +0000

    deploy: c023d7024de5be298e66a17561ea9c51b5fd7850

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-//const fs = require("fs");
-//const NOT_FOUND_PATH = "_site/404.html";
-
-module.exports = 
-	function(eleventyConfig) {
-		eleventyConfig.addPassthroughCopy("css");
-		eleventyConfig.addPassthroughCopy("img");
-	};
